d918ddf5b7cb7e8713eae6ceefdc212352a70718
devronyrfcl <95331585+devronyrfcl@users.noreply.github.com>
2023-08-06 16:04:10 +0600

    123

--- Assets/Script/BirdController.cs
+++ Assets/Script/BirdController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BirdController : MonoBehaviour
 {
     public float forwardSpeed = 5.0f; // Speed at which the bird moves forward
+    public float horizontalSpeed = 3.0f; // Speed at which the bird moves left/right
     public float tiltSensitivity = 2.0f; // Sensitivity of tilt control
 
     private Rigidbody rb;
@@ -17,16 +18,21 @@ public class BirdController : MonoBehaviour
     private void Update()
     {
         // Move the bird forward
-        Vector3 forwardMovement = transform.forward * forwardSpeed * Time.deltaTime;
+        Vector3 forwardMovement = -transform.forward * forwardSpeed * Time.deltaTime; // Negative z-axis
         rb.MovePosition(rb.position + forwardMovement);
 
         // Tilt control for Android
         if (Input.acceleration != Vector3.zero)
         {
             Vector3 tiltInput = Quaternion.Euler(90, 0, 0) * Input.acceleration;
-            Vector3 targetRotation = Quaternion.Euler(0, Mathf.Atan2(tiltInput.x, tiltInput.z) * Mathf.Rad2Deg, 0).eulerAngles;
+            Vector3 targetRotation = Quaternion.Euler(0, Mathf.Atan2(tiltInput.x, tiltInput.y) * Mathf.Rad2Deg, 0).eulerAngles; // Adjusted for y-axis tilt
             Quaternion tiltRotation = Quaternion.Slerp(rb.rotation, Quaternion.Euler(targetRotation), tiltSensitivity * Time.deltaTime);
             rb.MoveRotation(tiltRotation);
         }
+
+        // Horizontal movement
+        float horizontalInput = Input.GetAxis("Horizontal"); // Get input from keyboard or touch controls
+        Vector3 horizontalMovement = transform.right * horizontalInput * horizontalSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + horizontalMovement);
     }
 }
